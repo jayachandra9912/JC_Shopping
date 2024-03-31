4e6021a5fdb8d5505933576e1e25c52b3490a3b5
jayachandra9912 <97786283+jayachandra9912@users.noreply.github.com>
2024-03-31 12:32:06 +0530

    first commit

---
+++ src/Components/DescriptionBox/DescriptionBox.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import './DescriptionBox.css'
+
+const DescriptionBox = () => {
+    return (
+        <div className='descriptionbox'>
+            <div className="descriptionbox-navigator">
+                <div className="descriptionbox-nav-box">Description</div>
+                <div className="descriptionbox-nav-box fade">Reviews (122)</div>
+            </div>
+            <div className="descriptionbox-description">
+                <p>Effective clothing description examples highlight the quality and features of the fabric used in tailoring an item. Whether it is made from organic materials, premium fabrics, or unique blends. Describe its softness, breathability, and durability to showcase the value of the garment. Describe the Style and Occasion.</p>
+                <p>Effective clothing description examples highlight the quality and features of the fabric used in tailoring an item. Whether it is made from organic materials, premium fabrics, or unique blends. Describe its softness, breathability, and durability to showcase the value of the garment. Describe the Style and Occasion.</p>
+            </div>
+        </div>
+    )
+}
+
+export default DescriptionBox
